79d30195efe13bd085f4f7773bdc32fb76673543
Tanopwan <tanopwan.tos@ascendcorp.com>
2017-02-27 22:07:16 +0700

    callback url

--- server/config/environment/production.config.js
+++ server/config/environment/production.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	facebook: {
 		clientID: '426640641003931',
 		clientSecret: 'ef1210b58c4bf2cef39f32018f1fec96',
-		callbackURL: `${process.env.DOMAIN}:${process.env.PORT}/api/auth/facebook/callback`
+		callbackURL: `${process.env.DOMAIN}/api/auth/facebook/callback`
 	},
 	mongo: {
 		uri: 'mongodb://fizbonemanager:fizadmin@ds157819.mlab.com:57819/heroku_w1b98w1f',
