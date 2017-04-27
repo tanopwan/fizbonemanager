bd4b841c3f64879f6f0b3e661dd2fd2f9828c465
Tanopwan <tanopwan.tos@ascendcorp.com>
2017-04-27 17:41:30 +0700

    fix

--- server/service/bot/session.js
+++ server/service/bot/session.js
@@ -5,7 +5,7 @@ const sessions = {};
 module.exports = {
 	createSession(senderID, recipientID, timeOfMessage) {
 		let key = `${recipientID}_${senderID}`;
-		let session = sessions.key;
+		let session = sessions[key];
 		if (!session) {
 			session = {
 				senderID,
@@ -15,18 +15,18 @@ module.exports = {
 					name: 'idle',
 				}
 			}
-			sessions.key = session;
+			sessions[key] = session;
 		}
 		console.log("Create User Session...");
 		console.log(sessions);
 	},
 	getSession(senderID, recipientID) {
 		let key = `${recipientID}_${senderID}`;
-		return sessions.key;
+		return sessions[key];
 	},
 	setState(senderID, recipientID, newState) {
 		let key = `${recipientID}_${senderID}`;
-		let session = sessions.key;
+		let session = sessions[key];
 		if (session) {
 			session.state = newState;
 		}
